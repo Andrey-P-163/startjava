9a67805a0f49769af8443eeb5b2d15642fb35a82
Andrey <Andrey.nsk.Pro@gmail.com>
2021-01-09 01:07:16 +0400

    HW add Variable

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,24 @@
+public class Variable{
+    public static void main(String[] agre){
+        byte MyByte = 4;
+        short MyShort = 4000;
+        int MyInt = 2807;
+        long MyLong = 6900;
+
+        float MyFloat = 3.10f;
+        double MyDouble = 2.9;
+
+        char MyChar = 'i';
+
+        boolean MyBoolean = true;
+
+        System.out.println(MyByte);
+        System.out.println(MyShort);
+        System.out.println(MyInt);
+        System.out.println(MyLong);
+        System.out.println(MyFloat);
+        System.out.println(MyDouble);
+        System.out.println(MyChar);
+        System.out.println(MyBoolean);
+    }
+}
