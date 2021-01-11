381fcea7f3c3de1af6e918a30b6fa9af4e23a4de
Andrey <Andrey.nsk.Pro@gmail.com>
2021-01-12 01:08:03 +0400

    Add Calculator

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,38 @@
+public class Calculator{
+    public static void main (String[] agre){
+
+        int numberOne = 2;
+        int numberTwo = 3;
+        int resultInt = 0;
+        double resultDouble = 0;
+        char symbol = '+';
+
+        if (symbol == '+') {
+            resultInt = numberOne + numberTwo;
+            System.out.println(numberOne + " " + symbol + " " + numberTwo + " = " + resultInt);
+        
+        } else if (symbol == '-') {
+            resultInt = numberOne - numberTwo;
+            System.out.println(numberOne + " " + symbol + " " + numberTwo + " = " + resultInt);
+        
+        } else if (symbol == '*') {
+            resultInt = numberOne * numberTwo;
+            System.out.println(numberOne + " " + symbol + " " + numberTwo + " = " + resultInt);
+        
+        } else if (symbol == '%') {
+            resultInt = numberOne % numberTwo;
+            System.out.println(numberOne + " " + symbol + " " + numberTwo + " = " + resultInt);
+        
+        } else if (symbol == '/') {
+            resultDouble = (double) numberOne / numberTwo;
+            System.out.println(numberOne + " " + symbol + " " + numberTwo + " = " + resultDouble);
+        
+        } else if (symbol == '^') {
+            resultInt = 1;
+            for (int i = 1; i <= numberTwo; i++) {
+                resultInt = resultInt * numberOne;
+            }
+            System.out.println(numberOne + " " + symbol + " " + numberTwo + " = " + resultInt);
+        }
+    }
+}
