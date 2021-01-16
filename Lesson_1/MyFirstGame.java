14a3cd785c296abbbe5701eb99746d19b4e27dbb
Andrey <Andrey.nsk.Pro@gmail.com>
2021-01-16 14:48:36 +0400

    Fix MyFirstGame

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,15 +1,17 @@
 public class MyFirstGame {
     public static void main(String[] agre) {
-        int randomNum = 0;
+        int randomNum = 10;
+        int numberPlayer = 0;
         while (true) {
-            int numberPlayer = 0;
             if (numberPlayer == randomNum) {
                 System.out.println("Вы победили");
                 break;
             } else if (numberPlayer > randomNum) {
                 System.out.println("Введенное вами число больше того, что загадал компьютер");
+                numberPlayer--;
             } else if (numberPlayer < randomNum) {
                 System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                numberPlayer++;
             }
         }
         System.out.println("До новых встреч");
