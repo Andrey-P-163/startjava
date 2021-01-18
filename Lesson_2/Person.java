fff6eb3e458d52f7a2ec6621241b11de797f5b77
Andrey <Andrey.nsk.Pro@gmail.com>
2021-01-18 22:58:26 +0400

    Add Person HW_Lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    String gender = "Man";
+    String name = "Ivan";
+    double height = 1.7;
+    double weight = 80;
+    int age = 32;
+
+    void walk() {
+    }
+
+    void sit() {
+    }
+
+    void run() {
+    }
+
+    String speak() {
+        return "Hi";
+    }
+
+    boolean learnJava() {
+        return true;
+    }
+}
