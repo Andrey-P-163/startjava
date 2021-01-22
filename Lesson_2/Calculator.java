c40c0ebd7f3ca932822044ec110e20bed44ddfd3
Andrey <Andrey.nsk.Pro@gmail.com>
2021-01-22 22:28:02 +0400

    Add Calculator, CalculatorTest HW_2

---
+++ Lesson_2/Calculator.java
@@ -0,0 +1,45 @@
+public class Calculator {
+        private int numberOne;
+        private int numberTwo;
+        private double result;
+        private char mathSign;
+
+        public void setNumberOne(int numberOne) {
+            this.numberOne = numberOne;
+        }
+
+        public void setNumberTwo(int numberTwo) {
+            this.numberTwo = numberTwo;
+        }
+
+        public void setMathSign(char mathSign) {
+            this.mathSign = mathSign;
+        }
+
+        public double getResult() {
+        switch(mathSign) {
+            case '+':
+                result = numberOne + numberTwo;
+                break;
+            case '-':
+                result = numberOne - numberTwo;
+                break;
+            case '*':
+                result = numberOne * numberTwo;
+                break;
+            case '%':
+                result = numberOne % numberTwo;
+                break;
+            case '/':
+                result = (double) numberOne / numberTwo;
+                break;
+            case '^':
+                result = 1;
+            for (int i = 1; i <= numberTwo; i++) {
+                result *= numberOne;
+            }
+            break;
+        }
+        return result;
+    }
+}
---
+++ Lesson_2/CalculatorTest.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class CalculatorTest{
+    public static void main(String[] agre) {
+        Calculator calculator = new Calculator();
+        Scanner dataInput = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Введите первое число: ");
+            int numberOne = dataInput.nextInt();
+            calculator.setNumberOne(numberOne);
+
+            System.out.print("Введите знак математической операции: ");
+            char mathSign = dataInput.next().charAt(0);
+            calculator.setMathSign(mathSign);
+
+            System.out.print("Введите второе число: ");
+            int numberTwo = dataInput.nextInt();
+            calculator.setNumberTwo(numberTwo);
+
+            System.out.println(numberOne + " " + mathSign + " " + numberTwo + " = " + calculator.getResult());
+            System.out.print("Хотите продолжить вычисления? [да/нет]: ");
+
+            int count = 0;
+            while (true) {
+                Scanner repeatInput = new Scanner(System.in);
+                String inputSolution = repeatInput.nextLine();
+                if (inputSolution.equals("Нет")) {
+                    count = 1;
+                    break;
+                } else if (inputSolution.equals("Да")) {
+                    break;
+                } else {
+                    System.out.print("Хотите продолжить вычисления? [да/нет]: ");
+                }
+            }
+            if (count == 1) {
+                break;
+            }
+        }
+    }
+}
