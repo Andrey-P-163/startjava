01373fa1ae642a7c012a75bf0b407d9ea50e3629
Andrey <Andrey.nsk.Pro@gmail.com>
2021-02-06 01:07:10 +0400

    Removing old structure

--- Lesson_1/Unicode.java
+++
@@ -1,7 +0,0 @@
-public class Unicode {
-    public static void main(String[] agre) {
-        for (char unicode = 9398; unicode <= 10178; unicode++) {
-            System.out.println(unicode);
-        }
-    }
-}
