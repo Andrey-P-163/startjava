54c6460c7efadb0a7536016df4cc64701f466ced
Andrey <Andrey.nsk.Pro@gmail.com>
2021-02-17 00:03:15 +0400

    fix GuessNumberTest, GuessNumber, Player

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -7,7 +7,6 @@ public class GuessNumber {
     private Player playerOne;
     private Player playerTwo;
 
-
     public GuessNumber(Player playerOne, Player playerTwo) {
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
